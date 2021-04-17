889334eb7058780730b6254ed06d7b1d483957ab
BrunoGomes10 <62894584+BrunoGomes10@users.noreply.github.com>
2021-04-17 08:24:41 -0400

    Initial commit

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,27 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) { }
+
+  execute({ email, name }: IRequest): User {
+
+    const userExists = this.usersRepository.findByEmail(email);
+
+    if (userExists) {
+      throw new Error("User already exits");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
+
+  }
+}
+
+export { CreateUserUseCase };
---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,26 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) { }
+
+  execute({ user_id }: IRequest): User[] {
+    const userAdmin = this.usersRepository.findById(user_id);
+
+    if (!userAdmin) {
+      throw new Error("User not exists !!!");
+    }
+
+    if (!userAdmin.admin) {
+      throw new Error("User not permission for process ...");
+    }
+
+    return this.usersRepository.list();
+  }
+}
+
+export { ListAllUsersUseCase };
